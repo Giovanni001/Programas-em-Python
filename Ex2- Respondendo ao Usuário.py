0a4b4a903eba24506d0c2def563acb879de89f93
Giovanni <giovannimanganotti001@gmail.com>
2019-03-20 07:10:44 -0300

    Exercicio 002

---
+++ Ex2- Respondendo ao Usuário.py
@@ -0,0 +1,8 @@
+'''
+Exercício Python 002: Faça um programa que leia o nome de uma pessoa e mostre
+                    uma mensagem de boas-vindas.
+'''
+
+nome = str(input('Digite o seu nome: '))
+
+print('Seja Bem Vindo Sr°',nome)
