1a3789e9f3bddfc7c6d2e2316c02958cf2bd6597
Giovanni <giovannimanganotti001@gmail.com>
2019-03-20 07:20:15 -0300

    Exercício 009

---
+++ Ex9-Tabuada.py
@@ -0,0 +1,19 @@
+'''
+Exercício Python 9: Faça um programa que leia um número Inteiro
+qualquer e mostre na tela a sua tabuada.
+'''
+
+
+num = int(input('Digite um numero para ver a sua tabuada: '))
+
+print(num, 'x 0  = ', num * 0)
+print(num, 'x 1  = ',num * 1)
+print(num, 'x 2  = ',num * 2)
+print(num, 'x 3  = ',num * 3)
+print(num, 'x 4  = ',num * 4)
+print(num, 'x 5  = ',num * 5)
+print(num, 'x 6  = ',num * 6)
+print(num, 'x 7  = ',num * 7)
+print(num, 'x 8  = ',num * 8)
+print(num, 'x 9  = ',num * 9)
+print(num, 'x 10  = ',num * 10)
