ba8c647ba9be4caa2cc436765c2be5627eaf2922
Giovanni <giovannimanganotti001@gmail.com>
2019-03-23 08:34:14 -0300

    Deleted files

--- Ex1-Olá Mundo.py
+++
@@ -1,5 +0,0 @@
-'''
-Exercício Python 001: Crie um programa que escreva Olá Mundo na tela.
-'''
-
-print('Olá Mundo')
--- Ex2- Respondendo ao Usuário.py
+++
@@ -1,8 +0,0 @@
-'''
-Exercício Python 002: Faça um programa que leia o nome de uma pessoa e mostre
-                    uma mensagem de boas-vindas.
-'''
-
-nome = str(input('Digite o seu nome: '))
-
-print('Seja Bem Vindo Sr°',nome)
