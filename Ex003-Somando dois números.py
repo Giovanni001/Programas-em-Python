968a229bcda06cb7221aca934f9334b793d37b83
Giovanni <giovannimanganotti001@gmail.com>
2019-03-23 08:41:42 -0300

    Exercício 003

---
+++ Ex003-Somando dois números.py
@@ -0,0 +1,11 @@
+'''
+Exercício Python 003: -  Crie um programa que leia dois
+                        números e mostre a soma entre eles.
+'''
+
+n1 = int(input('Digite o 1° numero: '))
+n2 = int(input('Digite o 2° numero: '))
+
+soma = n1 + n2
+
+print(f'A soma entre {n1} e {n2} é de {soma}')
