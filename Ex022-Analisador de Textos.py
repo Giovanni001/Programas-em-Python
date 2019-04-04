8bc6bb4700a432b48c3519f3e04355ae7657da57
Giovanni <giovannimanganotti001@gmail.com>
2019-04-04 19:55:06 -0300

    Exercício 0022

--- Ex022-Analisador de Textos.py
+++ Ex022-Analisador de Textos.py
@@ -1,5 +1,5 @@
 '''
-Exercício Python 022: Crie um programa que leia o nome completo de uma pessoa e mostre:
+Exercício Python 022:  Crie um programa que leia o nome completo de uma pessoa e mostre:
 - O nome com todas as letras maiúsculas e minúsculas.
 - Quantas letras tem ao total sem considerar espaços.
 - Quantas letras tem o primeiro nome.
