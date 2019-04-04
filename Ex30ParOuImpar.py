74b2bc4400d0c14bb52246ea9ec1fd00d712a693
Giovanni <giovannimanganotti001@gmail.com>
2019-04-04 20:22:54 -0300

    Exercício 030

---
+++ Ex30ParOuImpar.py
@@ -0,0 +1,21 @@
+'''
+Exercício Python 030: Crie um programa que leia um número inteiro e
+mostre na tela se ele é PAR ou ÍMPAR.
+'''
+
+num = int(input('Digite um numero: '))
+print('PAR' if num % 2 == 0 else 'Impar')
+
+#---------------------------------------------------
+
+num = int(input('Digite um numero: '))
+
+if num % 2 == 0:
+    print('O numero digitado é par')
+else:
+    print('O numero digitado é impar')
+    
+    
+    
+
+
