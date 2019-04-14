9dd877e3a3b1d4c141b7e10d11572bc44c06fa6b
Giovanni <giovannimanganotti001@gmail.com>
2019-04-14 15:35:26 -0300

    Exercício 057

---
+++ Ex057-Validação de Dados.py
@@ -0,0 +1,12 @@
+'''
+Exercício Python 057: Faça um programa que leia o sexo de uma pessoa,
+mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação
+novamente até ter um valor correto.
+'''
+
+resp = str(input('Digite o seu sexo [M/F]: ')).strip()[0]
+
+while resp not in 'MmFf':
+    resp = str(input('Dados Invalidos. Digite novamente! '))
+print('Cadastrado com Sucesso !')
+
